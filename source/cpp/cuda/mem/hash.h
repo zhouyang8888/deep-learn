3454359bdce719460d96d6f2bce90fca6e12c745
zhouyang <blasterzhou@163.com>
2018-01-08 05:03:00 +0800

    FIX: hash table bugs.

--- source/cpp/cuda/mem/hash.h
+++ source/cpp/cuda/mem/hash.h
@@ -24,25 +24,25 @@
 #include <malloc.h>
 #include <cstdint>
 
-class hashkey {
+class hash_key {
 public:
-    virtual uint64_t hash_code() = 0;
-    virtual bool operator==(const hashkey& other) = 0;
+    virtual uint64_t hash_code() const =0;
+    virtual bool operator==(const hash_key& other) const =0;
 };
 
-template<class value>
+template<class hashkey, class value>
 struct node {
 	hashkey key;
 	value val;
-	inline node(hashkey& key, value& val) : key(key), val(val){};
+	inline node(const hashkey& key, const value& val) : key(key), val(val){};
 
 	node* next;
 };
 
-template<class value>
+template<class hashkey, class value>
 class hash {
 	private:
-		node<value>** table;
+		node<hashkey, value>** table;
 		int size;
 		int cnt;
 
@@ -50,21 +50,21 @@ class hash {
         float rate;
 	public:
 		inline hash(int size=100, float rate=0.7f): min_size(size), size(size), rate(rate), cnt(0){
-			table = (node<value>**) malloc(sizeof(node<value>*) * (size + 1));
-			memset(table, 0, sizeof(node<value>*) * (size + 1));
+			table = (node<hashkey, value>**) malloc(sizeof(node<hashkey, value>*) * (size + 1));
+			memset(table, 0, sizeof(node<hashkey, value>*) * (size + 1));
 		}
 
 	private:
-		inline int offset(hashkey& key) {
-			uint64_t addrv = key.hash_code();
-			addrv >>= 2;
+		inline int offset(const hashkey& key) const {
+            const hash_key& hkey = dynamic_cast<const hash_key&>(key);
+			uint64_t addrv = hkey.hash_code();
 			return (addrv % size) + 1;
 		}
 
 	public:
-		inline value* get(hashkey& key) {
+		inline const value * get(const hashkey& key) const {
 			int o = offset(key);
-			node<value>* pp = table[o];
+			node<hashkey, value>* pp = table[o];
 			while (pp) {
 				if (pp->key == key) {
 					return &pp->val;
@@ -74,9 +74,9 @@ class hash {
 			}
 			return NULL;
 		}
-		inline bool insert(hashkey& key, value& val) {
+		inline bool insert(const hashkey& key, const value& val) {
 			int o = offset(key);
-			node<value>* pp = table[o];
+			node<hashkey, value>* pp = table[o];
 			while (pp) {
 				if (pp->key == key) {
 					return false;
@@ -85,26 +85,26 @@ class hash {
 				}
 			}
 
-			node<value>* pnode = new node<value>(key, val);
+			node<hashkey, value>* pnode = new node<hashkey, value>(key, val);
 			pnode->next = table[o];
 			table[o] = pnode;
 			++cnt;
 			if (cnt > size * rate) expand();
 			return true;
 		}
-		inline bool remove(hashkey& key) {
+		inline bool remove(const hashkey& key) {
 			int o = offset(key);
-			node<value>** pp = table + o;
+			node<hashkey, value>** pp = table + o;
 			while (*pp) {
 				if ((*pp)->key == key) {
-					node<value>* todelete = *pp;
+					node<hashkey, value>* todelete = *pp;
 					*pp = (*pp)->next;
 					delete todelete;
 					--cnt;
 					if (cnt < size * (1 - rate) && size > min_size) shrink();
 					return true;
 				} else {
-					*pp = (*pp)->next;
+					pp = &(*pp)->next;
 				}
 			}
 			return false;
@@ -113,16 +113,16 @@ class hash {
 	private:
 		inline void expand() {
 			int oldsize = size;
-			node<value>** oldtable = table;
+			node<hashkey, value>** oldtable = table;
 
 			size <<= 1;
-			table = (node<value>**) malloc(sizeof(node<value>*) * (size + 1));
-			memset(table, 0,  sizeof(node<value>*) * (size + 1));
+			table = (node<hashkey, value>**) malloc(sizeof(node<hashkey, value>*) * (size + 1));
+			memset(table, 0,  sizeof(node<hashkey, value>*) * (size + 1));
 
 			for (int i = 1; i <= oldsize; ++i) {
-				node<value>* pnode = oldtable[i];
+				node<hashkey, value>* pnode = oldtable[i];
 				while (pnode) {
-					node<value>* pnext = pnode->next;
+					node<hashkey, value>* pnext = pnode->next;
 
 					int o = offset(pnode->key);
 					pnode->next = table[o];
@@ -137,16 +137,16 @@ class hash {
 
 		inline void shrink() {
 			int oldsize = size;
-			node<value>** oldtable = table;
+			node<hashkey, value>** oldtable = table;
 
 			size >>= 1;
-			table = (node<value>**) malloc(sizeof(node<value>*) * (size + 1));
-			memset(table, 0,  sizeof(node<value>*) * (size + 1));
+			table = (node<hashkey, value>**) malloc(sizeof(node<hashkey, value>*) * (size + 1));
+			memset(table, 0,  sizeof(node<hashkey, value>*) * (size + 1));
 
 			for (int i = 1; i <= oldsize; ++i) {
-				node<value>* pnode = oldtable[i];
+				node<hashkey, value>* pnode = oldtable[i];
 				while (pnode) {
-					node<value>* pnext = pnode->next;
+					node<hashkey, value>* pnext = pnode->next;
 
 					int o = offset(pnode->key);
 					pnode->next = table[o];
